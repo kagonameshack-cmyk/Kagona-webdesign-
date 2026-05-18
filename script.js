b5a187ff27ee5282809bd0de93945ea745bac41d
kagonameshack-cmyk <kagonameshack@gmail.com>
2026-05-19 00:44:29 +0300

    script.js

--- script.js
+++ script.js
@@ -2,8 +2,29 @@ const menuToggle = document.querySelector(".menu-toggle");
 
 const mobileMenu = document.querySelector(".mobile-menu");
 
+const mobileLinks =
+document.querySelectorAll(".mobile-menu a");
+
+/* TOGGLE MENU */
+
 menuToggle.addEventListener("click", () => {
 
     mobileMenu.classList.toggle("active");
+
+    menuToggle.classList.toggle("active");
+
+});
+
+/* CLOSE AFTER CLICK */
+
+mobileLinks.forEach(link => {
+
+    link.addEventListener("click", () => {
+
+        mobileMenu.classList.remove("active");
+
+        menuToggle.classList.remove("active");
+
+    });
 
 });
