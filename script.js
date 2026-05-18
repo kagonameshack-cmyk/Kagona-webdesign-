5e4a70e12ac650acc14fe0d6f590fc9b2013c749
kagonameshack-cmyk <kagonameshack@gmail.com>
2026-05-18 23:39:07 +0300

    script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const menuToggle = document.querySelector(".menu-toggle");
+
+const mobileMenu = document.querySelector(".mobile-menu");
+
+menuToggle.addEventListener("click", () => {
+
+    mobileMenu.classList.toggle("active");
+
+});
