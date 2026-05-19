7b53b748d1b5afc7b046663f2e05f5ddbe57ee08
kagonameshack-cmyk <kagonameshack@gmail.com>
2026-05-19 10:27:34 +0300

    script.js

--- script.js
+++ script.js
@@ -34,3 +34,30 @@ mobileLinks.forEach(link => {
     });
 
 });
+
+
+const reveals = document.querySelectorAll(
+'.projects-title, .project-card, .philosophy-left, .philosophy-item, .services-title, .service-card, .process-title, .process-card, .contact-left, .contact-form'
+);
+
+window.addEventListener('scroll', revealSections);
+
+function revealSections(){
+
+    const trigger =
+    window.innerHeight * 0.88;
+
+    reveals.forEach(item => {
+
+        const top =
+        item.getBoundingClientRect().top;
+
+        if(top < trigger){
+            item.classList.add('active');
+        }
+
+    });
+
+}
+
+revealSections();
